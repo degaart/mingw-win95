3fe66acf363b8dfddd37f56546563786a00116bd
degaart <degaart@users.noreply.github.com>
2025-10-07 20:23:00 +0300

    Win95 patch

--- build.sh
+++ build.sh
@@ -12,6 +12,8 @@ PREFIX="$BASEDIR/$ARCH"
 BUILDDIR="$BASEDIR/build"
 TARBALLSDIR="$BASEDIR/tarballs"
 STATEDIR="$BASEDIR/state"
+#NPROC=$(nproc)
+NPROC=8
 
 export PATH="$PREFIX/bin:$PATH"
 
@@ -65,6 +67,7 @@ TARBALLS=( \
     https://ftpmirror.gnu.org/gnu/gmp/gmp-6.3.0.tar.xz \
     https://ftpmirror.gnu.org/gnu/mpc/mpc-1.3.1.tar.gz \
     https://ftpmirror.gnu.org/gnu/mpfr/mpfr-4.2.2.tar.xz \
+    http://gcc.gnu.org/pub/gcc/infrastructure/isl-0.18.tar.bz2 \
     https://downloads.sourceforge.net/project/mingw-w64/mingw-w64/mingw-w64-release/mingw-w64-v13.0.0.tar.bz2 \
 )
 
@@ -78,6 +81,141 @@ do
 done
 
 mkdir -p "$BUILDDIR"
+
+### gmp
+if ! extract gmp-6.3.0.tar.xz GMP_EXTRACT_DIR > "$STATEDIR/gmp-extract.log" 2>&1; then
+    tail "$STATEDIR/gmp-extract.log"
+    exit 1
+fi
+
+if ! checkstate gmp-installed; then
+    echo "Configuring gmp"
+    if ! "$GMP_EXTRACT_DIR/configure" \
+        --prefix="$PREFIX" \
+        --enable-static \
+        --disable-shared \
+        > "$STATEDIR/gmp-configure.log" 2>&1
+    then
+        tail "$STATEDIR/gmp-configure.log"
+        exit 1
+    fi
+
+    echo "Building gmp"
+    if ! make -j$NPROC > "$STATEDIR/gmp-build.log" 2>&1; then
+        tail "$STATEDIR/gmp-build.log"
+        exit 1
+    fi
+
+    echo "Installing gmp"
+    if ! make install > "$STATEDIR/gmp-install.log" 2>&1; then
+        tail "$STATEDIR/gmp-install.log"
+        exit 1
+    fi
+    writestate gmp-installed
+fi
+
+### mpfr
+if ! extract mpfr-4.2.2.tar.xz MPFR_EXTRACT_DIR > "$STATEDIR/mpfr-extract.log" 2>&1; then
+    tail "$STATEDIR/mpfr-extract.log"
+    exit 1
+fi
+
+if ! checkstate mpfr-installed; then
+    echo "Configuring mpfr"
+    if ! "$MPFR_EXTRACT_DIR/configure" \
+        --prefix="$PREFIX" \
+        --enable-static \
+        --disable-shared \
+        --disable-dependency-tracking \
+        --with-gmp="$PREFIX" \
+        > "$STATEDIR/mpfr-configure.log" 2>&1
+    then
+        tail "$STATEDIR/mpfr-configure.log"
+        exit 1
+    fi
+
+    echo "Building mpfr"
+    if ! make -j$NPROC > "$STATEDIR/mpfr-build.log" 2>&1; then
+        tail "$STATEDIR/mpfr-build.log"
+        exit 1
+    fi
+
+    echo "Installing mpfr"
+    if ! make install > "$STATEDIR/mpfr-install.log" 2>&1; then
+        tail "$STATEDIR/mpfr-install.log"
+        exit 1
+    fi
+    writestate mpfr-installed
+fi
+
+### mpc
+if ! extract mpc-1.3.1.tar.gz MPC_EXTRACT_DIR > "$STATEDIR/mpc-extract.log" 2>&1; then
+    tail "$STATEDIR/mpc-extrac.log"
+    exit 1
+fi
+
+if ! checkstate mpc-installed; then
+    echo "Configuring mpc"
+    if ! "$MPC_EXTRACT_DIR/configure" \
+        --prefix="$PREFIX" \
+        --enable-static \
+        --disable-shared \
+        --disable-dependency-tracking \
+        --with-gmp="$PREFIX" \
+        --with-mpfr="$PREFIX" \
+        > "$STATEDIR/mpc-configure.log" 2>&1
+    then
+        tail "$STATEDIR/mpc-configure.log"
+        exit 1
+    fi
+
+    echo "Building mpc"
+    if ! make -j$NPROC > "$STATEDIR/mpc-build.log" 2>&1; then
+        tail "$STATEDIR/mpc-build.log"
+        exit 1
+    fi
+
+    echo "Installing mpc"
+    if ! make install > "$STATEDIR/mpc-install.log" 2>&1; then
+        tail "$STATEDIR/mpc-install.log"
+        exit 1
+    fi
+    writestate mpc-installed
+fi
+
+### isl
+if ! extract isl-0.18.tar.bz2 ISL_EXTRACT_DIR > "$STATEDIR/isl-extract.log" 2>&1; then
+    tail "$STATEDIR/isl-extract.log"
+    exit 1
+fi
+
+if ! checkstate isl-installed; then
+    echo "Configuring isl"
+    if ! "$ISL_EXTRACT_DIR/configure" \
+        --prefix="$PREFIX" \
+        --enable-static \
+        --disable-shared \
+        --disable-dependency-tracking \
+        --with-gmp-prefix="$PREFIX" \
+        > "$STATEDIR/isl-configure.log" 2>&1
+    then
+        tail "$STATEDIR/isl-configure.log"
+        exit 1
+    fi
+
+    echo "Building isl"
+    if ! make -j$NPROC > "$STATEDIR/isl-build.log" 2>&1; then
+        tail "$STATEDIR/isl-build.log"
+        exit 1
+    fi
+
+    echo "Installing isl"
+    if ! make install > "$STATEDIR/isl-install.log" 2>&1; then
+        tail "$STATEDIR/isl-install.log"
+        exit 1
+    fi
+    writestate isl-installed
+fi
 
 ### binutils
 if ! extract binutils-2.45.tar.xz BINUTILS_EXTRACT_DIR > "$STATEDIR/binutils-extract.log" 2>&1; then
@@ -96,14 +234,27 @@ if ! checkstate binutils-installed; then
         --disable-multilib \
         --disable-nls \
         --enable-lto \
-        --disable-gdb > "$STATEDIR/binutils-configure.log" 2>&1
+        --disable-gdb \
+        --disable-werror \
+        --enable-gold \
+        --enable-ld \
+        --disable-dependency-tracking \
+        --disable-libquadmath \
+        --disable-libquadmath-support \
+        --enable-plugins \
+        --with-gmp="$PREFIX" \
+        --with-mpc="$PREFIX" \
+        --with-mpfr="$PREFIX" \
+        --with-isl="$PREFIX" \
+        --with-static-standard-libraries \
+        > "$STATEDIR/binutils-configure.log" 2>&1
     then
         tail "$STATEDIR/binutils-configure.log"
         exit 1
     fi
 
     echo "Building binutils"
-    if ! make -j$(nproc) > "$STATEDIR/binutils-build.log" 2>&1; then
+    if ! make -j$NPROC > "$STATEDIR/binutils-build.log" 2>&1; then
         tail "$STATEDIR/binutils-build.log"
         exit 1
     fi
@@ -118,21 +269,39 @@ fi
 
 ### mingw-w64 headers
 if ! extract mingw-w64-v13.0.0.tar.bz2 MINGW_EXTRACT_DIR > "$STATEDIR/mingw-extract.log" 2>&1; then
-    patch -d "$MINGW_EXTRACT_DIR" -p1 "$BASEDIR/patches/winpthreads-disable_debugger_check.diff"
     tail "$STATEDIR/mingw-extract.log"
     exit 1
+fi
+
+if ! checkstate mingw-w64-patched; then
+    patch -d "$MINGW_EXTRACT_DIR" -p1 < "$BASEDIR/patches/winpthreads-disable_debugger_check.diff"
+    writestate mingw-w64-patched
 fi
 
 if ! checkstate mingw-w64-installed; then
     echo "Configuring mingw-w64-headers"
     if ! "$MINGW_EXTRACT_DIR/mingw-w64-headers/configure" \
         --host="$ARCH" \
-        --prefix="$PREFIX" \
         --with-default-win32-winnt=0x0400 \
         --with-default-msvcrt=msvcrt-os \
-        --prefix="$PREFIX/$ARCH" > "$STATEDIR/mingw-headers.log" 2>&1
+        --prefix="$PREFIX/mingw" \
+        CFLAGS="-DWIN32_WINNT=0x0400 -DWINVER=0x0400" \
+        CXXFLAGS="-DWIN32_WINNT=0x0400 -DWINVER=0x0400" \
+        > "$STATEDIR/mingw-headers.log" 2>&1
     then
         tail "$STATEDIR/mingw-headers-build.log"
+        exit 1
+    fi
+
+    echo "Building mingw-w64-headers"
+    if ! make -j$NPROC > "$STATEDIR/mingw-headers-build.log" 2>&1; then
+        tail "$STATEDIR/mingw-headers-build.log"
+        exit 1
+    fi
+
+    echo "Installing mingw-w64-headers"
+    if ! make install > "$STATEDIR/mingw-headers-install.log" 2>&1; then
+        tail "$STATEDIR/mingw-headers-install.log"
         exit 1
     fi
 
@@ -150,6 +319,11 @@ if ! extract gcc-15.2.0.tar.xz GCC_EXTRACT_DIR > "$STATEDIR/gcc-extract.log" 2>&
     exit 1
 fi
 
+if ! checkstate gcc-patched; then
+    patch -d "$GCC_EXTRACT_DIR" -p1 < "$BASEDIR/patches/gcc-remove_getthreadid.diff"
+    writestate gcc-patched
+fi
+
 if ! checkstate gcc-phase1-installed; then
     echo "Configuring gcc-phase1"
     if ! "$GCC_EXTRACT_DIR/configure" \
@@ -161,14 +335,25 @@ if ! checkstate gcc-phase1-installed; then
         --enable-languages=c,c++ \
         --disable-nls \
         --enable-threads=posix \
-        --disable-sjlj-exceptions \
-        --with-dwarf2 \
-        --enable-version-specific-runtime-libs \
-        --disable-libstdcxx-verbose \
         --disable-dependency-tracking \
-        --disable-lto \
-        CFLAGS_FOR_TARGET="-Os" \
-        CXXFLAGS_FOR_TARGET="-Os" \
+        --with-pic \
+        --enable-gold \
+        --enable-ld \
+        --enable-lto \
+        --disable-bootstrap \
+        --disable-libssp \
+        --disable-libquadmath \
+        --disable-libquadmath-support \
+        --with-gmp="$PREFIX" \
+        --with-mpc="$PREFIX" \
+        --with-mpfr="$PREFIX" \
+        --with-isl="$PREFIX" \
+        --with-sysroot="$PREFIX" \
+        --with-build-sysroot="$PREFIX" \
+        --with-as="$PREFIX/bin/${ARCH}-as" \
+        --with-ld="$PREFIX/bin/${ARCH}-ld" \
+        CFLAGS_FOR_TARGET="-Os -DWIN32_WINNT=0x0400 -DWINVER=0x0400" \
+        CXXFLAGS_FOR_TARGET="-Os -DWIN32_WINNT=0x0400 -DWINVER=0x0400" \
         LDFLAGS_FOR_TARGET="-s" \
         > "$STATEDIR/gcc-phase1-configure.log" 2>&1
     then
@@ -177,7 +362,7 @@ if ! checkstate gcc-phase1-installed; then
     fi
 
     echo "Building gcc-phase1"
-    if ! make -j$(nproc) all-gcc > "$STATEDIR/gcc-phase1-build.log" 2>&1; then
+    if ! make -j$NPROC all-gcc > "$STATEDIR/gcc-phase1-build.log" 2>&1; then
         tail "$STATEDIR/gcc-phase1-build.log"
         exit 1
     fi
@@ -203,7 +388,8 @@ if ! checkstate mingw-w64-crt-installed; then
         --with-sysroot="$PREFIX/$ARCH" \
         --enable-lib32 \
         --disable-lib64 \
-        CFLAGS="-Os" \
+        --disable-dependency-tracking \
+        CFLAGS="-Os -DWIN32_WINNT=0x0400 -DWINVER=0x0400" \
         LDFLAGS="-s" \
         > "$STATEDIR/mingw-crt-configure.log" 2>&1
     then
@@ -212,7 +398,7 @@ if ! checkstate mingw-w64-crt-installed; then
     fi
 
     echo "Building mingw-w64-crt"
-    if ! make -j$(nproc) > "$STATEDIR/mingw-crt-build.log" 2>&1; then
+    if ! make -j$NPROC > "$STATEDIR/mingw-crt-build.log" 2>&1; then
         tail "$STATEDIR/mingw-crt-build.log"
         exit 1
     fi
@@ -236,7 +422,7 @@ if ! checkstate winpthreads-installed; then
         --disable-shared \
         --enable-static \
         --prefix="$PREFIX/$ARCH" \
-        CFLAGS="-Os" \
+        CFLAGS="-Os -DWIN32_WINNT=0x0400 -DWINVER=0x0400" \
         LDFLAGS="-s" \
         > "$STATEDIR/winpthreads-configure.log" 2>&1
     then
@@ -245,7 +431,7 @@ if ! checkstate winpthreads-installed; then
     fi
 
     echo "Building winpthreads"
-    if ! make -j$(nproc) > "$STATEDIR/winpthreads-build.log" 2>&1; then
+    if ! make -j$NPROC > "$STATEDIR/winpthreads-build.log" 2>&1; then
         tail "$STATEDIR/winpthreads-build.log"
         exit 1
     fi
@@ -261,10 +447,48 @@ fi
 
 ### gcc (phase 2)
 if ! checkstate gcc-phase2-installed; then
+    rm -rf "${GCC_EXTRACT_DIR}-build"
+    mkdir "${GCC_EXTRACT_DIR}-build"
     cd "${GCC_EXTRACT_DIR}-build"
 
+    echo "Configuring gcc-phase2"
+    if ! "$GCC_EXTRACT_DIR/configure" \
+        --target="$ARCH" \
+        --disable-shared \
+        --enable-static \
+        --disable-multilib \
+        --prefix="$PREFIX" \
+        --enable-languages=c,c++ \
+        --disable-nls \
+        --enable-threads=posix \
+        --disable-dependency-tracking \
+        --with-pic \
+        --enable-gold \
+        --enable-ld \
+        --enable-lto \
+        --disable-bootstrap \
+        --disable-libssp \
+        --disable-libquadmath \
+        --disable-libquadmath-support \
+        --with-gmp="$PREFIX" \
+        --with-mpc="$PREFIX" \
+        --with-mpfr="$PREFIX" \
+        --with-isl="$PREFIX" \
+        --with-sysroot="$PREFIX" \
+        --with-build-sysroot="$PREFIX" \
+        --with-as="$PREFIX/bin/${ARCH}-as" \
+        --with-ld="$PREFIX/bin/${ARCH}-ld" \
+        CFLAGS_FOR_TARGET="-Os -DWIN32_WINNT=0x0400 -DWINVER=0x0400" \
+        CXXFLAGS_FOR_TARGET="-Os -DWIN32_WINNT=0x0400 -DWINVER=0x0400" \
+        LDFLAGS_FOR_TARGET="-s" \
+        > "$STATEDIR/gcc-phase1-configure.log" 2>&1
+    then
+        tail "$STATEDIR/gcc-phase1-configure.log"
+        exit 1
+    fi
+
     echo "Building gcc-phase2"
-    if ! make -j$(nproc) > "$STATEDIR/gcc-phase2-build.log" 2>&1; then
+    if ! make -j$NPROC > "$STATEDIR/gcc-phase2-build.log" 2>&1; then
         tail "$STATEDIR/gcc-phase2-build.log"
         exit 1
     fi
